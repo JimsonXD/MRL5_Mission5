efec7b5eaa23c9a68cb2c5dca4836c26fe2a64da
Jimson Montederamos <jimson@Jimsons-MacBook-Pro.local>
2023-09-12 12:43:11 +1200

    fixed conflicts from app.tsx

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from "./commonComponents/Main";
 import Home from "./pages/home/Home";
-import RentalPropertySearch from "./pages/rental-property-search/RentalPropertySearch";
-import PropertyDetails from "./pages/property-details/PropertyDetails";
-import MyTenancyApplication from "./pages/my-tenancy-application/MyTenancyApplication";
-import Application from "./pages/application/Application";
-import ConfirmationDetails from "./pages/confirmation-details/ConfirmationDetails";
+// import RentalPropertySearch from "./pages/rental-property-search/RentalPropertySearch";
+// import PropertyDetails from "./pages/property-details/PropertyDetails";
+// import MyTenancyApplication from "./pages/my-tenancy-application/MyTenancyApplication";
+// import Application from "./pages/application/Application";
+// import ConfirmationDetails from "./pages/confirmation-details/ConfirmationDetails";
 
 
 
@@ -14,13 +15,15 @@ function App() {
     <Router>
       <Routes>
 
+
         <Route path="/" Component={Home} />
-        <Route path="/rental-property-search" Component={RentalPropertySearch} />
+        {/* <Route path="/rental-property-search" Component={RentalPropertySearch} />
         <Route path="/property-details" Component={PropertyDetails} />
         <Route path="/my-tenancy-application" Component={MyTenancyApplication} />
         <Route path="/application" Component={Application} />
-        <Route path="/confirmation-details" Component={ConfirmationDetails} />
-        
+        <Route path="/confirmation-details" Component={ConfirmationDetails} /> */}
+         <Route path="/week1" Component={Main } /> 
+
 
     </Routes>
     </Router>
