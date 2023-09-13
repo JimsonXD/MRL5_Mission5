a56f35d00ce7b4b6ddc6f137fcf49b9ca146fbc7
John Doe <patricktricenio549@gmail.com>
2023-09-13 21:48:00 +1200

    homepage

---
+++ react/src/pages/home/CarouselHomePage.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import HomePageCarousel from '../../assets/HomePageCarousel.png'
+function CarouselHomePage() {
+  return (
+    <Carousel data-bs-theme="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-100 h-25"
+          src={HomePageCarousel}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          {/* <h5>First slide label</h5> */}
+          <h3>Changing to Metro NZ Property Management is easy. We collect the required information from you and in return, you’ll see the benefits of working with a people-focused company. We have an enviable reputation for working with landlords, renters, building owners and property investors and property managers.</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={HomePageCarousel}
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          {/* <h5>Second slide label</h5> */}
+          <h3>Changing to Metro NZ Property Management is easy. We collect the required information from you and in return, you’ll see the benefits of working with a people-focused company. We have an enviable reputation for working with landlords, renters, building owners and property investors and property managers.</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={HomePageCarousel}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          {/* <h5>Third slide label</h5> */}
+          <h3>Changing to Metro NZ Property Management is easy. We collect the required information from you and in return, you’ll see the benefits of working with a people-focused company. We have an enviable reputation for working with landlords, renters, building owners and property investors and property managers.</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default CarouselHomePage;
--- react/src/pages/home/Home.tsx
+++ react/src/pages/home/Home.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Navbar from "../../commonComponents/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CarouselHomePage from '../home/CarouselHomePage';
 import Footer from "../../commonComponents/Footer";
-import HomePagePicture from '../../assets/HomePage.png'
+import HomePagePicture from '../../assets/HomePage.png';
+import BodyCorporateHomepage from '../../assets/BodyCorporateAdministration.png'
+import styles from '../../pages/home/Home.module.css';
+
+
+
 
 
 const Home = () => {
   
-  const products = [
-    {
-      id: 1,
-      imageSrc: HomePagePicture,
-      imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
-    },
-  ]
+  
   
   
   
@@ -33,29 +34,167 @@ const Home = () => {
         </div>
 
         {/* Picture */}
-        <div className="bg-white">
-          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-            <h2 className="sr-only">Products</h2>
+        <div className={styles.flexContainer1}>
+  <div className={styles.flexItemleft1}>
+    <div className={styles.imageContainer}>
+      <img
+        src={HomePagePicture}
+        style={{
+          maxWidth: '100%',
+          height: 'auto',
+          display: 'block',
+        }}
+        alt="Your Image"
+      />
+    </div>
+    <div className={styles.flexItemrightitle}>
+      <br></br>
+      <br></br>
+      <p>Property Management Auckland</p>
+      <p>
+        Metro NZ is one of the top Auckland Property Management companies. All
+        of our staff are property investors who have expert knowledge of the
+        property market and your best interest in mind at all times. Check out
+        our tailored services we offer as an Auckland property management
+        company.
+        <div className={styles.buttoncontainer}>
+        <button className={styles.redbutton}>Property Owners</button>
+        <button className={styles.redbutton}>Rental Listings</button>
+        <button className={styles.redbutton}>My Tenancy Application</button>
+    </div>
 
-            <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-              {products.map((product) => (
-                <a key={product.id} href='' className="group">
-                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                    <img
-                      src={product.imageSrc}
-                      alt={product.imageAlt}
-                      className="h-full w-full object-cover object-center group-hover:opacity-75"
-                    />
-                  </div>
-                </a>
-              ))}
-            </div>
-          </div>
-        </div>
+      </p>
+    </div>
+  </div>
+</div>
 
+        <div className={styles.SecondSlogan}>Helping Clients To Achieve Harmony Through Our Expertise and Skills</div>
         {/* flexbox */}
-        
+        <div className={styles.flexContainer}>
+      <div className={styles.flexItemleft}>Property Management<img
+      src={BodyCorporateHomepage}
+    /></div>
+      <div className={styles.flexItemright}>Body Corporate Administration<img
+      src={BodyCorporateHomepage}
+      
+     
+    /></div>
+      <div className={styles.flexItemright}>Building Management<img
+      src={BodyCorporateHomepage}
+    /></div>
+      <div className={styles.flexItemright}>Decluttering Management<img
+      src={BodyCorporateHomepage}
+    /></div>
+      <div className={styles.flexItemright}>Free Property Appraisals<img
+      src={BodyCorporateHomepage}
+    /></div>
+      <div className={styles.flexItemright}>Renovation Property Management<img
+      src={BodyCorporateHomepage}
+    /></div>
+      <div className={styles.flexItemright}>Investment Properties<img
+      src={BodyCorporateHomepage}
+    /></div>
+      <div className={styles.flexItemright}>Rental Properties<img
+      src={BodyCorporateHomepage}
+    /></div>
+    </div>
+    <div className={styles.buttoncontainer}>
+        <button className={styles.redbutton}>Property Owners</button>
+        <button className={styles.redbutton}>Rental Listings</button>
+    </div>
+
+
+    <div className={styles.SecondSlogan}>Working With Landlords, Tenants,Building Owners and Property Investors</div>
+    {/* Carousel */}
+  
+      <CarouselHomePage/>
+      <div className={styles.buttoncontainer}>
+        <button className={styles.redbutton}>Call Today</button>
+        <button className={styles.redbutton}>Email Us</button>
+    </div>
+
+    {/*  Testimonial Page*/}
+
     
+    <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div className={styles.SecondSlogan}>Testimonials</div>
+      <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
+      <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white " />
+      <div className="mx-auto max-w-2xl lg:max-w-4xl">
+       
+      
+        <figure className="mt-10">
+          <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+            <p>
+                                          
+            Everything was smooth and efficient, which i really appreciate. I will be recommending them to others looking for home. My experience with NZ metro property was really good from start to finish. 
+            </p>
+          </blockquote>
+          <figcaption className="mt-10">
+            <img
+              className="mx-auto h-10 w-10 rounded-full"
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+              <div className="font-semibold text-gray-900">Vanessa Jepson</div>
+              <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
+                <circle cx={1} cy={1} r={1} />
+              </svg>
+              <div className="text-gray-600">posted on 23-08-202</div>
+            </div>
+          </figcaption>
+        </figure>
+      </div>
+    </section>
+
+
+    {/* Awards Section */}
+
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+         Awards
+        </h2>
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+            alt="Transistor"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+            alt="Reform"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+            alt="Tuple"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+            alt="SavvyCal"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+            alt="Statamic"
+            width={158}
+            height={48}
+          />
+        </div>
+      </div>
+    </div>
       </div>
       <Footer />
     </div>
