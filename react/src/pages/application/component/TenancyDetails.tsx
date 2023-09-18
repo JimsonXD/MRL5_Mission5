95699c1af237425e54f86e37468e2973512583a5
John Doe <patricktricenio549@gmail.com>
2023-09-18 15:28:52 +1200

    tenancydetails

--- react/src/pages/application/component/TenancyDetails.tsx
+++ react/src/pages/application/component/TenancyDetails.tsx
@@ -1,70 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuestion } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom"
 
 const TenancyDetails = () => {
+  // Create a state object to manage form data
+  const [formData, setFormData] = useState({
+    addressOfTenancy: "",
+    rent: "",
+    bond: "",
+    firstName: "",
+    lastName: "",
+    accountDetails: "",
+    bankName: "",
+    signedByTenant: "",
+    dateSignedByTenant: "",
+    signedByLandlord: "",
+    dateSignedByLandlord: "",
+  });
+
+  // Handle input changes and update form data
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      // Send form data to the server
+      await axios.post("http://localhost:8080/api/submittenancydetails", formData);
+      // Redirect to a confirmation page or perform other actions as needed
+     
+    } catch (error) {
+      console.error("An error occurred while submitting the form:", error);
+    }
+  };
+
   return (
-    
     <div className="flex flex-col justify-center px-24 bg-white p-8">
-      
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="space-y-12 bg-gray-100 p-6 rounded-lg shadow-md">
           <div className="border-b border-gray-300 pb-12">
             <h2 className="text-lg font-semibold text-gray-800">
               Tenancy Details
             </h2>
-            
 
             <div className="grid grid-cols-1 gap-8">
               <div>
                 <input
                   type="text"
-                  name="address-of-tenancy"
-                  id="address-of-tenancy"
+                  name="addressOfTenancy"
+                  id="addressOfTenancy"
                   autoComplete="address-of-tenancy"
                   placeholder="Address of Tenancy"
                   className="border-2 bg-white block w-full rounded-md border-gray-300 py-2 px-3 text-gray-800 shadow-sm placeholder-gray-400 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
+                  onChange={handleChange}
                 />
               </div>
 
-
-
-<div className="flex w-full">
-              <div className="mt-2 w-1/2 pr-24">
-                <input
-                  type="text"
-                  name="rent"
-                  id="rent"
-                  autoComplete="rent"
-                  placeholder="$ Rent"
-                  className="border-2 bg-white block w-full rounded-md border-gray-300 py-2 px-3 text-gray-800 shadow-sm placeholder-gray-400 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
-                />
+              <div className="flex w-full">
+                <div className="mt-2 w-1/2 pr-24">
+                  <input
+                    type="text"
+                    name="rent"
+                    id="rent"
+                    autoComplete="rent"
+                    placeholder="$ Rent"
+                    className="border-2 bg-white block w-full rounded-md border-gray-300 py-2 px-3 text-gray-800 shadow-sm placeholder-gray-400 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mt-4 space-x-4 gap-24 w-full px-8">
+                  <label className="gap-8 space-x-8 items-center">
+                    <input
+                      type="radio"
+                      name="paymentFrequency"
+                      value="weekly"
+                      
+                    />
+                    Weekly
+                  </label>
+                  <label className="p-16">
+                    <input
+                      type="radio"
+                      name="paymentFrequency"
+                      value="fortnightly"
+                  
+                    />
+                    Fortnightly
+                  </label>
+                  <label>
+                    <input
+                      type="radio"
+                      name="paymentFrequency"
+                      value="monthly"
+                
+                    />
+                    Monthly
+                  </label>
+                </div>
               </div>
-              <div className="mt-4 space-x-4 gap-24 w-full px-8">
-                <label className="gap-8 space-x-8 items-center">
-                  <input type="radio" name="payment-frequency" value="weekly" />
-                  Weekly
-                </label>
-                <label className="p-16">
-                  <input
-                    type="radio"
-                    name="payment-frequency"
-                    value="fortnightly"
-                  />
-                  Fortnightly
-                </label>
-                <label>
-                  <input
-                    type="radio"
-                    name="payment-frequency"
-                    value="monthly"
-                  />
-                  Monthly
-                </label>
-              </div></div>
-
-
             </div>
           </div>
 
@@ -73,9 +111,13 @@ const TenancyDetails = () => {
           </h2>
 
           <div className="flex gap-32">
-            <label className="flex items-center space-x-8 gap-2 ">
-              <input type="radio" name="tenancy-type" value="fixed"/>
-              
+            <label className="flex items-center space-x-8 gap-2">
+              <input
+                type="radio"
+                name="tenancyType"
+                value="fixed"
+
+              />
               Fixed
               <div className="w-6 h-6 flex items-center justify-center rounded-full bg-red-500">
                 <FontAwesomeIcon
@@ -85,7 +127,12 @@ const TenancyDetails = () => {
               </div>
             </label>
             <label className="flex items-center space-x-8 gap-2">
-              <input type="radio" name="tenancy-type" value="periodic" />
+              <input
+                type="radio"
+                name="tenancyType"
+                value="periodic"
+
+              />
               Periodic
               <div className="w-6 h-6 flex items-center justify-center rounded-full bg-red-500">
                 <FontAwesomeIcon
@@ -104,6 +151,7 @@ const TenancyDetails = () => {
               autoComplete="bond"
               placeholder="$ Bond"
               className="border-2 bg-white block w-full rounded-md border-gray-300 py-2 px-3 text-gray-800 shadow-sm placeholder-gray-400 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
+              onChange={handleChange}
             />
           </div>
 
@@ -111,23 +159,26 @@ const TenancyDetails = () => {
             Rent to be paid at
           </h2>
 
-
-
-
           <div className="flex space-x-12 gap-12">
             <label className="flex items-center gap-2">
-              <input type="radio" name="tenancy-type" value="fixed" />
+              <input
+                type="radio"
+                name="landlordAddress"
+                value="Landlord physical address"
+        
+              />
               Landlord physical address
             </label>
             <label className="flex items-center gap-2">
-              <input type="radio" name="tenancy-type" value="periodic" />
+              <input
+                type="radio"
+                name="bankTransfer"
+                value="Bank Transfer"
+
+              />
               Bank Transfer
             </label>
           </div>
-
-
-
-
 
           <h2 className="text-lg font-semibold text-gray-800 mb-6">
             Bank Details
@@ -137,21 +188,23 @@ const TenancyDetails = () => {
             <div>
               <input
                 type="text"
-                name="first-name"
-                id="first-name"
+                name="firstName"
+                id="firstName"
                 autoComplete="given-name"
                 placeholder="First Name"
                 className="border-2 bg-white block w-full rounded-md border-gray-300 py-2 px-3 text-gray-800 shadow-sm placeholder-gray-400 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
+                onChange={handleChange}
               />
             </div>
             <div>
               <input
                 type="text"
-                name="last-name"
-                id="last-name"
+                name="lastName"
+                id="lastName"
                 autoComplete="family-name"
                 placeholder="Last Name"
                 className="border-2 bg-white block w-full rounded-md border-gray-300 py-2 px-3 text-gray-800 shadow-sm placeholder-gray-400 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -160,21 +213,23 @@ const TenancyDetails = () => {
             <div>
               <input
                 type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
-                placeholder="First Name"
+                name="accountDetails"
+                id="accountDetails"
+                autoComplete="account-details"
+                placeholder="Account Details"
                 className="border-2 bg-white block w-full rounded-md border-gray-300 py-2 px-3 text-gray-800 shadow-sm placeholder-gray-400 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
+                onChange={handleChange}
               />
             </div>
             <div>
               <input
                 type="text"
-                name="last-name"
-                id="last-name"
-                autoComplete="family-name"
-                placeholder="Last Name"
+                name="bankName"
+                id="bankName"
+                autoComplete="bank-name"
+                placeholder="Bank Name"
                 className="border-2 bg-white block w-full rounded-md border-gray-300 py-2 px-3 text-gray-800 shadow-sm placeholder-gray-400 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -214,21 +269,23 @@ const TenancyDetails = () => {
             <div>
               <input
                 type="text"
-                name="signed-by-tenant"
-                id="signed-by-tenant"
-                autoComplete="sign-by-tenant"
+                name="signedByTenant"
+                id="signedByTenant"
+                autoComplete="signed-by-tenant"
                 placeholder="Signed by Tenant"
                 className="border-2 bg-white block w-full rounded-md border-gray-300 py-2 px-3 text-gray-800 shadow-sm placeholder-gray-400 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
+                onChange={handleChange}
               />
             </div>
             <div>
               <input
                 type="text"
-                name="date-signed-by-tenant"
-                id="date-signed-by-tenant"
+                name="dateSignedByTenant"
+                id="dateSignedByTenant"
                 autoComplete="date-signed-by-tenant"
                 placeholder="Date signed by tenant"
                 className="border-2 bg-white block w-full rounded-md border-gray-300 py-2 px-3 text-gray-800 shadow-sm placeholder-gray-400 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -237,21 +294,23 @@ const TenancyDetails = () => {
             <div>
               <input
                 type="text"
-                name="signed-by-landlord"
-                id="signed-by-landlord"
+                name="signedByLandlord"
+                id="signedByLandlord"
                 autoComplete="signed-by-landlord"
                 placeholder="Signed by Landlord"
                 className="border-2 bg-white block w-full rounded-md border-gray-300 py-2 px-3 text-gray-800 shadow-sm placeholder-gray-400 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
+                onChange={handleChange}
               />
             </div>
             <div>
               <input
                 type="text"
-                name="date-signed-by-landlord"
-                id="date-signed-by-landlord"
+                name="dateSignedByLandlord"
+                id="dateSignedByLandlord"
                 autoComplete="date-signed-by-landlord"
                 placeholder="Date signed by Landlord"
                 className="border-2 bg-white block w-full rounded-md border-gray-300 py-2 px-3 text-gray-800 shadow-sm placeholder-gray-400 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -261,16 +320,14 @@ const TenancyDetails = () => {
               Save for later
             </button>
 
-
-            <Link to="/confirmation-details">
+            
+            
             <button 
             className="btn btn-primary bg-red-600 px-16 text-white">
               Submit
             </button>
-            </Link>
 
-
-
+           
           </div>
         </div>
       </form>
