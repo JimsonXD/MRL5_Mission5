ecfadfb874e299671ac793932146f4377d234442
Jimson Montederamos <jimson@Jimsons-MacBook-Pro.local>
2023-09-22 08:22:05 +1200

    added test

---
+++ express/tests/bookingController.spec.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const Booking = require("../models/bookingModel");
+const { bookToView } = require("../controllers/bookingController");
+
+describe("bookToView Function Unit Test", () => {
+    beforeAll(async () => {
+        await mongoose.connect("mongodb://localhost:27017/testdb", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    });
+
+    afterAll(async () => {
+        await mongoose.disconnect();
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("should create a new booking", async () => {
+        // Mock request and response objects
+        const req = {
+            body: {
+                firstName: "John",
+                lastName: "Doe",
+                phoneNumber: "1234567890",
+                email: "johndoe@example.com",
+                selectedDate: "2023-10-01",
+                selectedTime: "10:00 AM",
+            },
+        };
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        const saveSpy = jest.spyOn(Booking.prototype, "save").mockResolvedValueOnce();
+
+        await bookToView(req, res);
+
+        expect(saveSpy).toHaveBeenCalledTimes(1); // Ensure that save method was called
+        expect(res.status).toHaveBeenCalledWith(201);
+        expect(res.json).toHaveBeenCalledWith({ message: "Booking saved successfully" });
+        expect(Booking).toHaveBeenCalledTimes(1); // Ensure that Booking constructor was called
+        expect(Booking.mock.calls[0][0]).toEqual({
+            firstName: "John",
+            lastName: "Doe",
+            phoneNumber: "1234567890",
+            email: "johndoe@example.com",
+            selectedDate: "2023-10-01",
+            selectedTime: "10:00 AM",
+        });
+    });
+});
