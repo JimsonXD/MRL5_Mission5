dcc661d388c88e93b34b41ba0821e472e64d9920
Jimson Montederamos <jimson@Jimsons-MacBook-Pro.local>
2023-09-22 09:03:18 +1200

    test6

--- express/jest.config.js
+++ express/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   preset: '@babel/preset-env',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest', 
   },
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
-  testRegex: '(/tests/.*\\.(test|spec))\\.(ts|tsx|js)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(/tests/.*\\.(test|spec))\\.(js|jsx)$', 
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'], 
 };
